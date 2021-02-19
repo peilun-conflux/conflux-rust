aff1acdf2089ff45d04cfdbbc71ced7f18a0ac8a
Peilun Li <peilun.li@conflux-chain.org>
2021-02-19 11:43:43 +0800

    Support long path for Windows.

--- build.rs
+++ build.rs
@@ -2,5 +2,6 @@ fn main() {
     if cfg!(target_os = "windows") {
         println!("cargo:rustc-link-lib=dylib=ShLwApi");
         println!("cargo:rustc-link-lib=dylib=Rpcrt4");
+        embed_resource::compile("conflux-manifest.rc");
     }
 }
