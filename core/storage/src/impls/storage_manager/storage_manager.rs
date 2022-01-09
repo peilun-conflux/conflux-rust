6aed0d94e2050c6b0730c8895a4301490451546b
Peilun Li <peilun.li@confluxnetwork.org>
2022-01-09 22:19:45 +0800

    Fix overflow.

--- core/storage/src/impls/storage_manager/storage_manager.rs
+++ core/storage/src/impls/storage_manager/storage_manager.rs
@@ -829,10 +829,12 @@ impl StorageManager {
         state_availability_boundary: &RwLock<StateAvailabilityBoundary>,
     ) -> Result<()>
     {
-        let additional_state_height_gap =
-            (self.storage_conf.additional_maintained_snapshot_count
-                * self.storage_conf.consensus_param.snapshot_epoch_count)
-                as u64;
+        let additional_state_height_gap = self
+            .storage_conf
+            .additional_maintained_snapshot_count
+            .saturating_mul(
+                self.storage_conf.consensus_param.snapshot_epoch_count,
+            ) as u64;
         let maintained_state_height_lower_bound =
             if confirmed_height > additional_state_height_gap {
                 confirmed_height - additional_state_height_gap
